45c294c110bb4ac350931847842c287c56e87892
albertomg98 <albertomaria.gizzi@gmail.com>
2026-02-18 20:32:06 +0100

    move firebase config to env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTu8064cn6DmbBkkummaiPlzrsIHCfyGw",
-  authDomain: "idealmente-c4819.firebaseapp.com",
-  projectId: "idealmente-c4819",
-  storageBucket: "idealmente-c4819.firebasestorage.app",
-  messagingSenderId: "497483703952",
-  appId: "1:497483703952:web:c24b31cbb8c3c426f59a15"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
